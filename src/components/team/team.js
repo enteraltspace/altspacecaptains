46d48453c7430dfb531d9e926aafb5f4c5dd8513
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2022-12-07 01:48:14 +0530

    Update team.js

--- src/components/team/team.js
+++ src/components/team/team.js
@@ -9,7 +9,7 @@ function Team(){
         < h3 className="team-text">
         Seshu Karthick
         </ h3 >
-        <h5 className="team-text">Founder</h5>
+        <div className="team-text">Founder</div>
         <a href="https://www.linkedin.com/in/seshukarthick/" target="_blank"><img src={require('./linkedinlogo.png?auto=compress')} className="linkedin-logo"/></a>
     </span>
     
@@ -20,7 +20,7 @@ function Team(){
         < h3 className="team-text">
         T.N.Rao
         </ h3 >
-        <h5 className="team-text">Investor</h5>
+        <div className="team-text">Investor</div>
         <a href="#" target="_blank"><img src={require('./linkedinlogo.png?auto=compress')} className="linkedin-logo"/></a>
     </span>
     </span>
@@ -30,7 +30,7 @@ function Team(){
         < h3 className="team-text">
         Gireesh
         </ h3>
-        <h5 className="team-text">Developer</h5>
+        <div className="team-text">Developer</div>
         <a href="https://www.linkedin.com/in/gireeshredy
 " target="_blank"><img src={require('./linkedinlogo.png?auto=compress')} className="linkedin-logo"/></a>
     </span>
@@ -41,8 +41,8 @@ function Team(){
         < h3 className="team-text">
         Dhanush Waran
         </ h3>
-        <h5 className="team-text">Designer
-</h5>
+        <div className="team-text">Designer
+</div>
 <a href="#" target="_blank"><img src={require('./linkedinlogo.png?auto=compress')} className="linkedin-logo"/> </a>
     </span>
     </span>
@@ -52,7 +52,7 @@ function Team(){
         < h3 className="team-text">
         Sachin
         </ h3>
-        <h5 className="team-text">Community</h5>
+        <div className="team-text">Community</div>
       
         <a href="https://www.linkedin.com/in/sachin-s-5724841b4/" target="_blank"><img src={require('./linkedinlogo.png')} className="linkedin-logo"/></a>
     </span>
@@ -64,7 +64,7 @@ function Team(){
         < h3 className="team-text">
         Kiran
         </ h3>
-        <h5 className="team-text">Marketer</h5>
+        <div className="team-text">Marketer</div>
         <a href="https://www.linkedin.com/in/kirangowtham/" target="_blank"><img src={require('./linkedinlogo.png?auto=compress')} className="linkedin-logo"/></a>
     </span>
     </span>

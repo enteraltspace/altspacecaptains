ff60a6dca6dc030041c6f1c4e9c22607ec82c45e
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2022-12-07 01:27:29 +0530

    Update welcome.js

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -7,12 +7,11 @@ function Welcome(){
     <div className="welcome-div1" id="home">
         <h1 className="welcome-heading">
         Enter ALT Space 
-        </h1>
-        <h1 className="welcome-heading1">as Captains</h1>
-        <h4 className="welcome-text">
+        <br>
+        as Captains</h1>
+        <div className="welcome-text">
         ALT Space Captains collection is made up of 9,999 randomly generated NFTs that exist on the Polygon blockchain. The holders of ALT Space Captains NFTs can participate in an exclusive forum that brings together Web3 builders under 1 roof. Keep in mind that all Captains have caps, and that's what makes them different from everyone else in ALT Space.
-
-        </h4>
+        </div>
     </div>
     <div className="welcome-div2">
         <Mint/>

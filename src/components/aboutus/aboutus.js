a6435db47a6dcc46427ef6b87fd7672278c9da7f
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2022-12-07 02:25:25 +0530

    Update aboutus.js

--- src/components/aboutus/aboutus.js
+++ src/components/aboutus/aboutus.js
@@ -13,9 +13,10 @@ const About=()=>{
        
         <div className="part2">
         <div className='about-text'> ALT Space is a parallel universe that has unique characters called Captains, Champs, Cartoons and more. You can unlock characters by minting them, and start collaborating for your Web3 initiatives in multiple ways.
-<br/>
+<br/><br/>
 The holders of ALT Space Captains NFTs represent experienced players in Web3 community. They will have special previledges to interact, hire talent, collaborate and network with people who share a similar vision.
-<br/>
+<br/><br/>
+Whether you manage a Web3 product, a service startup, a blockchain company, or a de-fi solution, you can grab our Captains NFT to engage with similar players in the ALT Space.
 </div>
 </div>
    

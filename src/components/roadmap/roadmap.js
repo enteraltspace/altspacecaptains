e6bfe5a2e376a04725ec17c2c864db65f8181d75
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2022-12-27 16:03:54 +0530

    Update roadmap.js

--- src/components/roadmap/roadmap.js
+++ src/components/roadmap/roadmap.js
@@ -10,12 +10,11 @@ function Roadmap(){
 <h3>1</h3>
 </div>
 <div className="rectangular-div">
-<p className="roadmap-text">Website Development</p>
-<p className="roadmap-text">Create NFT Collection</p>
-<p className="roadmap-text">Private Sale Open</p>
-<p className="roadmap-text">Mint NFTs</p>
-<p className="roadmap-text">Build Community</p>
-           
+<p className="roadmap-text"><strong>PHASE 1 - Completed</strong></p>
+<p className="roadmap-text">&check; Website Development</p>
+<p className="roadmap-text">&check; Create NFT Collection</p>
+<p className="roadmap-text">&check; Partnership with Global Events</p>
+<p className="roadmap-text">&check; Private Sale Open - 1000 NFTs</p>          
 </div>
 </div>
 <div className="main-div">
@@ -24,9 +23,11 @@ function Roadmap(){
 <h3>2</h3>
 </div>
 <div className="rectangular-div">
-<p className="roadmap-text">Marketplace Listings</p>
-<p className="roadmap-text">Airdrop NFTs of New Collections</p>
+<p className="roadmap-text"><strong>PHASE 2</strong></p>
+<p className="roadmap-text">Launch of B2B Web3 Roster</p>
+<p className="roadmap-text">Launch the Wall of Fame</p>
 <p className="roadmap-text">Exclusive Portal for Collaboration</p>
+<p className="roadmap-text">Organize Online Summits</p>
 </div>
     </div>
     <div className="main-div">
@@ -35,8 +36,9 @@ function Roadmap(){
 <h3>3</h3>
 </div>
 <div className="rectangular-div">
+<p className="roadmap-text"><strong>PHASE 3</strong></p>
 <p className="roadmap-text">NFT Staking</p>
-<p className="roadmap-text">NFT Passive incomes</p>
+<p className="roadmap-text">Airdrop NFTs of New Collections</p>
 <p className="roadmap-text">New Utilities for NFT holders</p>
 <p className="roadmap-text">Interact in Metaverse</p>
 </div>

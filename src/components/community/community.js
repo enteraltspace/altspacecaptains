d9236752f30572ee4e8f8dfb11b4bf88b9981185
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2022-12-27 16:57:47 +0530

    Update community.js

--- src/components/community/community.js
+++ src/components/community/community.js
@@ -9,8 +9,9 @@ function Community(){
         <a href=" https://discord.gg/ru6g942RKe" target="_blank"><button className="community-button">JOIN DISCORD</button></a>
     </div>
     <div className="community-div1">
-        <img  src={require("./circles.png")} className="community-img1"/>
+        <img src={require("./circles.png")} className="community-img1"/>
     </div>
+    <img src={require("./Captains-Tokenomics.png")}/>
     </>)
 }
 export default Community

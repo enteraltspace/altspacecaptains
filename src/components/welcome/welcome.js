986110ff019e9e0f0726259b225c29d37a2e0fdb
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2023-02-08 07:27:15 +0530

    Update welcome.js

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -11,7 +11,7 @@ function Welcome(){
         as Captains</h2>
         <div className="welcome-text">
         <p>ALT Space Captains collection is made up of 9,999 randomly generated NFTs that exist on the Polygon blockchain. 
-        The holders of Captains NFTs can participate in an exclusive token-gated forum that brings together Web3 builders under 1 roof. 
+        The holders of Captains NFTs can participate in an exclusive token-gated lounge that brings together Web3 builders under 1 roof. 
         Keep in mind that all Captains have caps, and that's what makes them different from everyone else in ALT Space.</p>
         
         <p>Timeline: We are having our <strong>private sale</strong> now with reduced pricing. The <strong>public launch</strong> will be on Mar 23, 2023.</p>

dca2d7e1a144c3e323771e64ed764e91e9189317
enteraltspace <119533151+enteraltspace@users.noreply.github.com>
2023-02-08 07:32:22 +0530

    Update roadmap.js

--- src/components/roadmap/roadmap.js
+++ src/components/roadmap/roadmap.js
@@ -10,7 +10,7 @@ function Roadmap(){
 <h3>1</h3>
 </div>
 <div className="rectangular-div">
-<div className="roadmap-text"><strong>PHASE 1 - Completed</strong></div>
+<div className="roadmap-text"><strong>PHASE 1 (Prelaunch) - Completed</strong></div>
 <div className="roadmap-text">✓ Website Development</div>
 <div className="roadmap-text">✓ Create NFT Collection</div>
 <div className="roadmap-text">✓ Partnership with Global Events</div>
@@ -24,11 +24,10 @@ function Roadmap(){
 <h3>2</h3>
 </div>
 <div className="rectangular-div">
-<div className="roadmap-text"><strong>PHASE 2 - May 2023</strong></div>
-<div className="roadmap-text">Launch of Token-Gated Community Forum</div>
+<div className="roadmap-text"><strong>PHASE 2 (Launch) - March 2023</strong></div>
+<div className="roadmap-text">Launch of Token-Gated Lounge</div>
 <div className="roadmap-text">Launch of Elevator Reels</div>
 <div className="roadmap-text">Launch the Wall of Fame</div>
-<div className="roadmap-text">Organize Online Summits</div>
 </div>
     </div>
     <div className="main-div">
@@ -37,11 +36,11 @@ function Roadmap(){
 <h3>3</h3>
 </div>
 <div className="rectangular-div">
-<div className="roadmap-text"><strong>PHASE 3 - Dec 2023</strong></div>
+<div className="roadmap-text"><strong>PHASE 3 - June 2023</strong></div>
 <div className="roadmap-text">NFT Staking</div>
 <div className="roadmap-text">Airdrop NFTs of New Collections</div>
 <div className="roadmap-text">New Utilities for NFT holders</div>
-<div className="roadmap-text">Interact in Metaverse</div>
+<div className="roadmap-text">Organize Virtual Summits</div>
 </div>
     </div>
     

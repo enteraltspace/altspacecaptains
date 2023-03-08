ec8db1510186e35830815758ded15c0694081d1c
enteraltspace <altspace18@gmail.com>
2023-03-08 22:06:30 +0530

    updated alert and timer removal

--- src/components/welcome/mint/mintButton.js
+++ src/components/welcome/mint/mintButton.js
@@ -1,85 +1,96 @@
 import React, { useEffect } from "react";
-import './mintButton.css'
-import {abi,contractAddress} from '../constants.js'
-import './data.env'
+import "./mintButton.css";
+import { abi, contractAddress } from "../constants.js";
+import "./data.env";
 import Web3 from "web3";
 import Web3Modal from "web3modal";
 
-export function MintButton({value}){
-    var mintValue=value
-    const MintNow=async()=>{
-        console.log(abi)
-        
-        console.log(contractAddress)
-       console.log(mintValue)
-      
-        const WalletConnectProvider = window.WalletConnectProvider.default;
-        let providerOptions = {
-            walletconnect: {
-              package: WalletConnectProvider,
-              options: {
-                infuraId: "b50bee145172497d9576a6f79b1209aa",
-                //infuraId:'JuKirzHWDP97kprdQEkmzv0X7J8mz64emhs4Os70',
-                chainId:137,
-                rpc: {
-                   1: "https://mainnet.infura.io/v3/b50bee145172497d9576a6f79b1209aa",
-                },
-              }
-            },
+export function MintButton({ value }) {
+  var mintValue = value;
+  const MintNow = async () => {
+    console.log(abi);
+
+    console.log(contractAddress);
+    console.log(mintValue);
+
+    const WalletConnectProvider = window.WalletConnectProvider.default;
+    let providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: "b50bee145172497d9576a6f79b1209aa",
+          //infuraId:'JuKirzHWDP97kprdQEkmzv0X7J8mz64emhs4Os70',
+          chainId: 137,
+          rpc: {
+            1: "https://mainnet.infura.io/v3/b50bee145172497d9576a6f79b1209aa",
+          },
+        },
+      },
+    };
+    console.log("function called");
+    const web3Modal = new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions, // required
+    });
+
+    const provider = await web3Modal.connect();
+    const web3 = new Web3(provider);
+    const account = web3.eth.getAccounts();
+    const contractInstance = new web3.eth.Contract(abi, contractAddress);
+    const mintPrice = await contractInstance.methods.mintPrice().call();
+    account.then((result) => {
+      console.log(result[0]);
+      const balance = web3.eth.getChainId();
+
+      balance.then((r) => {
+        if (r == 137) {
+          const contractInstance = new web3.eth.Contract(abi, contractAddress);
+          let txTransfer = {
+            from: result[0],
+            to: contractAddress,
+            //  gas: web3.utils.toHex(web3.utils.toWei( '.028' , 'gwei' )),
+            maxPriorityFeePerGas: 80000000000,
+            maxFeePerGas: 250000000000,
+            value: value * mintPrice,
+            //                      gas:21000,
+            data: contractInstance.methods.mint(mintValue).encodeABI(),
           };
-        console.log('function called')
-        const web3Modal = new Web3Modal({
-            network: "mainnet", // optional
-            cacheProvider: true, // optional
-             providerOptions// required
-          });
-          
-           const provider = await web3Modal.connect();
-          const web3 =new Web3(provider);
-          const account=web3.eth.getAccounts()
-           const contractInstance= new web3.eth.Contract(abi,contractAddress)
-           const mintPrice = await contractInstance.methods.mintPrice().call();
-          account.then((result)=>{
-              console.log(result[0])
-              const balance=web3.eth.getChainId()
-             
-              balance.then((r)=>{
-                  if (r==137) {
-                    const contractInstance= new web3.eth.Contract(abi,contractAddress)
-                    let txTransfer = {
-                      from: result[0],
-                      to: contractAddress,
-                    //  gas: web3.utils.toHex(web3.utils.toWei( '.028' , 'gwei' )),
-                     maxPriorityFeePerGas:40000000000,
-                      maxFeePerGas:250000000000,
-                     value:value*mintPrice,
-//                      gas:21000,
-                      data: contractInstance.methods.mint(mintValue).encodeABI()
-                  }
-                  
-                  let approve=web3.eth.sendTransaction(txTransfer);
-                  approve.then((result)=>{
-                      let message = `NFT Mint Sucessfull https://polygonscan.com/tx/${result.transactionHash}`;
-                      alert(message);
-                  }).catch((e)=>{
-                      console.error(e)
-                      alert(e.message)
-                  })     
-                  }
-        
-                  else{
-                      alert("Please connect to polygon chain")
-                  }
-                   
-                
-              })
+
+          web3.eth
+            .sendTransaction(txTransfer)
+            .on("transactionHash", function (hash) {
+              if (
+                window.confirm("Ok to go to polygonscan, Cancel to Stay here")
+              ) {
+                window.open(`https://polygonscan.com/tx/${hash}`, "_blank");
+              }
+
+              // alert(`NFT Mint Successful https://polygonscan.com/tx/${hash}`);
             })
-            
-    }
-    return(<>
-    
-    <button className="mint-button" onClick={MintNow}>
+            .on("error", function (error) {
+              alert(error);
+            });
+          // let approve = web3.eth.sendTransaction(txTransfer);
+          //   approve
+          //     .then((receipt) => {
+          //       let message = `NFT Mint Sucessfull https://polygonscan.com/tx/${receipt.transactionHash}`;
+          //       alert(message);
+          //     })
+          //     .catch((e) => {
+          //       alert(e.message);
+          //     });
+          // } else {
+          alert("Please connect to polygon chain");
+        }
+      });
+    });
+  };
+  return (
+    <>
+      <button className="mint-button" onClick={MintNow}>
         Mint Now
-    </button>
-    </>)
+      </button>
+    </>
+  );
 }
--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -1,27 +1,34 @@
 import React from "react";
-import './welcome.css'
+import "./welcome.css";
 import Mint from "./mint/mint.js";
-function Welcome(){
-    return(
+function Welcome() {
+  return (
     <>
-    <div className="welcome-div1" id="home">
-        <h1 className="welcome-heading">
-        Enter ALT Space </h1>
-        <h2 className="welcome-heading1">
-        as Captains</h2>
+      <div className="welcome-div1" id="home">
+        <h1 className="welcome-heading">Enter ALT Space </h1>
+        <h2 className="welcome-heading1">as Captains</h2>
         <div className="welcome-text">
-        <p>ALT Space Captains collection is made up of 9,999 randomly generated NFTs that exist on the Polygon blockchain. 
-        The holders of Captains NFTs can participate in an exclusive token-gated lounge that brings together Web3 builders under 1 roof. 
-        Keep in mind that all Captains have caps, and that's what makes them different from everyone else in ALT Space.</p>
-        
-        <p>Timeline: We are having our <strong>private sale</strong> now with reduced pricing. The <strong>public launch</strong> will be on Mar 23, 2023.</p>
+          <p>
+            ALT Space Captains collection is made up of 9,999 randomly generated
+            NFTs that exist on the Polygon blockchain. The holders of Captains
+            NFTs can participate in an exclusive token-gated lounge that brings
+            together Web3 builders under 1 roof. Keep in mind that all Captains
+            have caps, and that's what makes them different from everyone else
+            in ALT Space.
+          </p>
+
+          <p>
+            Timeline: We are having our <strong>private sale</strong> now with
+            reduced pricing. The <strong>public launch</strong> will be on Mar
+            23, 2023.
+          </p>
         </div>
-        <br/>
-    </div>
-    <div className="welcome-div2">
-        <Mint/>
-    </div>
+        <br />
+      </div>
+      <div className="welcome-div2">
+        <Mint />
+      </div>
     </>
-    )
+  );
 }
-export default Welcome
+export default Welcome;
